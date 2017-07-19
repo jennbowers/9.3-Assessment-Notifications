bb7b9014fb82f148a12b71bda1345b3b1c3f7545
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-19 15:34:44 -0400

    Added TextNotification class, created instance variables, the constructor, getters, and a transport method that overrides the Notification transport method.

---
+++ src/TextNotification.java
@@ -0,0 +1,26 @@
+/**
+ * Created by jenniferbowers on 7/19/17.
+ */
+public class TextNotification extends Notification {
+    private String recipient;
+    private String smsProvider;
+
+    public TextNotification(String subject, String body, String recipient, String smsProvider) {
+        super(subject, body);
+        this.recipient = recipient;
+        this.smsProvider = smsProvider;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSmsProvider() {
+        return smsProvider;
+    }
+
+    @Override
+    public void transport() {
+        System.out.println("Recipient: " + this.recipient + ", sms Provider: " + this.smsProvider + ", Subject: " + getSubject() + ", Body: " + getBody() + ", Created At: " + getCreatedAt());
+    }
+}
