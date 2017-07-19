325a089eed1995ed5bc4ed3ec821f2291e6203a2
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-19 15:30:35 -0400

    Added EmailNotification class, created instance variables, the constructor, getters, and a transport method that overrides the Notification transport method.

---
+++ src/EmailNotification.java
@@ -0,0 +1,26 @@
+/**
+ * Created by jenniferbowers on 7/19/17.
+ */
+public class EmailNotification extends Notification{
+    private String recipient;
+    private String smtpProvider;
+
+    public EmailNotification(String subject, String body, String recipient, String smtpProvider) {
+        super(subject, body);
+        this.recipient = recipient;
+        this.smtpProvider = smtpProvider;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSmtpProvider() {
+        return smtpProvider;
+    }
+
+    @Override
+    public void transport() {
+        System.out.println("Recipient: " + this.recipient + ", smtp Provider: " + this.smtpProvider + ", Subject: " + getSubject() + ", Body: " + getBody() + ", Created At: " + getCreatedAt());
+    }
+}
